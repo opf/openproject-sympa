5a5633bf482be045c7a2c4387a53462e576c728a
Markus Kahl <machisuji@gmail.com>
2018-02-01 13:22:01 +0000

    fixed simp mailing list creation and deletion

--- lib/open_project/sympa/actions.rb
+++ lib/open_project/sympa/actions.rb
@@ -1,34 +1,26 @@
 require 'open_project/sympa/logger'
+require 'open_project/sympa/actions/local_actions'
+require 'open_project/sympa/actions/remote_actions'
 
 module OpenProject
   module Sympa
     module Actions
-      def self.execute_command(command)
-        Logger.info("  executing #{command}")
-        system "sudo #{command} >> #{Logger.path} 2>&1 &"
+      class << self
+        def remote?
+          Setting.plugin_openproject_sympa['sympa_path'].start_with? "ssh "
+        end
+
+        def delegate_to
+          if remote?
+            OpenProject::Sympa::Actions::Remote
+          else
+            OpenProject::Sympa::Actions::Local
+          end
+        end
       end
 
-      def self.get_sympa_path
-        Setting.plugin_openproject_sympa['sympa_path']
-      end
-
-      def self.get_domain
-        Setting.plugin_openproject_sympa['sympa_domain']
-      end
-
-      def self.create_list(project)
-        temp_file = File.open("#{Rails.root}/tmp/list#{project.identifier}", "w+")
-        File.chmod(0644, temp_file.path)
-        temp_file.print(project.sympa_mailing_list_xml_def)
-        temp_file.flush
-        Logger.info "Creating mailing list for project #{project.identifier}"
-        execute_command("#{get_sympa_path} --create_list --robot #{get_domain} --input_file #{temp_file.path}")
-      end
-
-      def self.destroy_list(project)
-        Logger.info "Destroying mailing list for project #{project.identifier}"
-        execute_command("#{get_sympa_path} --purge_list=#{project.identifier}@#{get_domain}")
-      end
+      delegate :create_list, to: delegate_to
+      delegate :destroy_list, to: delegate_to
     end
   end
 end
